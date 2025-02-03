97b26baa884d30f1d6d1f9be5923f125769ae5d9
Prince Peter Yalley <136192065+pkyalley@users.noreply.github.com>
2025-02-02 19:04:56 -0500

    My first try on tkinter in Python

---
+++ tkinter_1.py
@@ -0,0 +1,33 @@
+# # my first try with tkinter
+# # developing GUIs (clothes) for all my naked codes
+# # i mean msot of them🤣🤣
+
+# from tkinter import *
+
+# root = Tk()
+
+# my_label = Label(root, text="Hello World!")
+
+# my_label.pack()
+
+# root.mainloop()
+
+from tkinter import *
+root = Tk()
+
+my_label = Label(root, text="""
+--------------------------------
+    My FIRST TRY WITH Tkinter
+--------------------------------
+                 
+My name is Prince Peter Yalley
+I am here to explore!
+                 
+Okay, Bye bye
+------------------------------
+        Peace✌️
+------------------------------""")
+
+my_label.pack()
+
+root.mainloop()
