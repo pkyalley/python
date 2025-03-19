5d69af6e730c035482e30a1f953127d70adfc4b4
Prince Peter Yalley <136192065+pkyalley@users.noreply.github.com>
2025-03-19 12:10:32 -0400

    Django 2 File

---
+++ django_2/storefront/playground/urls.py
@@ -0,0 +1,13 @@
+# from django.urls import path
+# from . import views
+
+# # URL Cinfiguration Model (URLconf)
+# urlpatterns = [
+#     path("hello/", views.say_hello)
+# ]
+
+a = "4"
+b = "5"
+c = a+b
+
+print(c)
